a490a99d636712be3ffd572dd67f424d62499869
DanielDi <despinalm@unal.edu.co>
2022-04-29 02:13:31 -0500

    App implementation

--- app.R
+++ app.R
@@ -9,40 +9,64 @@
 
 library(shiny)
 library(caret)
+load("App/modeloSatisfaccion.RData")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("Predicción de la Satisfacción"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("nvSalud",
+                  "Satisfacción de Salud",
+                  min = 0,
+                  max = 10,
+                  value = 1),
+      sliderInput("nvSeguridad",
+                  "Satisfacción de Seguridad",
+                  min = 0,
+                  max = 10,
+                  value = 1),
+      sliderInput("nvEducacion",
+                  "Satisfacción de Educación",
+                  min = 1,
+                  max = 9,
+                  value = 1),
+      sliderInput("condHogar",
+                  "Condiciones del Hogar",
+                  min = 1,
+                  max = 4,
+                  value = 1),
+      sliderInput("nvEducativo",
+                  "Nivel Educativo",
+                  min = 1,
+                  max = 9,
+                  value = 1)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      textOutput("prediccion")
+      # plotOutput("distPlot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  output$prediccion <- renderText({
+    entrada <- data.frame(X1=input$nvSalud,
+                          X2=input$nvSeguridad,
+                          X3=input$nvEducacion,
+                          X4=input$condHogar,
+                          X5=input$nvEducativo)
+    salida <- predict(lm1,newdata = entrada)
+    return(salida)
+    
+  })
 }
 
 # Run the application 
