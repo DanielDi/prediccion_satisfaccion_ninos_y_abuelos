293728ba454fc5b35aabeed54825896b3033b154
DanielDi <despinalm@unal.edu.co>
2022-05-01 18:36:25 -0500

    App Modelos abuelos

--- App/app.R
+++ App/app.R
@@ -13,6 +13,7 @@ library(shinythemes)
 source('modelosAbuelosUI.R')
 load("data/modeloSatisfaccion.RData")
 load("data/modeloSatisfaccionSalud.RData")
+load("data/modeloSatisfaccionSeguridad.RData")
 
 ui <- fluidPage(theme=shinytheme("superhero"),
                 navbarPage(title = "Nombre Proyecto",
@@ -32,24 +33,47 @@ ui <- fluidPage(theme=shinytheme("superhero"),
                 ))
 
 server <- function(input, output) {
+  
   # Modelo Salud Abuelos
-  output$prediccionSalud <- renderText({
-    entrada <- data.frame(CALIDAD_EPS=as.integer(input$calidadEps),
+  resultadoSalud <- reactive({
+    entradaSalud <- data.frame(CALIDAD_EPS=as.integer(input$calidadEps),
                           ESTADO_SALUD=as.integer(input$estadoSalud),
                           ESTRATO=as.integer(input$estrato),
-                          REGIMEN=as.numeric(input$regimen),
+                          REGIMEN=as.integer(input$regimen),
                           ENFERMEDAD_CRONICA=as.integer(input$enfermedadCronica))
-    salida <- predict(lm_salud, newdata = entrada)
+    salidaSalud <- predict(lm_salud, newdata = entradaSalud)
+    return(salidaSalud)
+  })
+  
+  output$prediccionSalud <- renderText({
+    salida <- resultadoSalud()
+  })
+  
+  # Modelo Seguridad Abuelos
+  
+  resultadoSeguridad <- reactive({
+    entrada <- data.frame(ESTADO_CIVIL=as.integer(input$estadoCivil),
+                          NIVEL_DE_SEGURIDAD=as.integer(input$nvSeguridad),
+                          CONDICIONES_DE_VIDA_HOGAR=as.integer(input$condHogar),
+                          ROBO=as.integer(input$robo),
+                          OTRO_DELITO=as.integer(input$otroDelito),
+                          LEE_ESCRIBE=as.integer(input$leeEscribe))
     
+    salida <- predict(lm_seguridad, newdata = entrada)
+    return(salida)
+  })
+  
+  output$prediccionSeguridad <- renderText({
+    salidaSeguridad <- resultadoSeguridad()
   })
   
   # Modelo General
   output$prediccion <- renderText({
-    entrada <- data.frame(X1=input$nvSalud,
-                          X2=input$nvSeguridad,
-                          X3=input$nvEducacion,
-                          X4=input$condHogar,
-                          X5=input$nvEducativo)
+    entrada <- data.frame(X1=resultadoSalud(),
+                          X2=resultadoSeguridad(),
+                          X3=as.integer(input$nvEducacion),
+                          X4=as.numeric(input$condHogar),
+                          X5=input$satisfaccionTrabajo)
     salida <- predict(lm1,newdata = entrada)
     return(salida)
     
